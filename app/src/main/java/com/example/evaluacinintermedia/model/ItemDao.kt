ef6630157719caa25765cbce228c9e5cd941df21
amunocis <amunocis@gmail.com>
2021-02-05 19:35:42 -0300

    feat: Added 2 model files -Entity (ItemEntity) -DAO (ItemDao)

--- app/src/main/java/com/example/evaluacinintermedia/model/ItemDao.kt
+++ app/src/main/java/com/example/evaluacinintermedia/model/ItemDao.kt
@@ -5,6 +5,27 @@ import androidx.room.*
 
 @Dao
 interface ItemDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertItem(item: ItemEntity)
 
-    suspend fun insertItem()
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAllItems(listItem: List<ItemEntity>)
+
+    @Update
+    suspend fun updateItem(item: ItemEntity)
+
+    @Delete
+    suspend fun deleteItem(item: ItemEntity)
+
+    @Query("DELETE FROM item_table")
+    suspend fun deleteAll()
+
+    @Query("SELECT * FROM item_table ORDER BY id DESC")
+    fun getAllItem(): LiveData<List<ItemEntity>>
+
+    @Query("SELECT * FROM item_table WHERE nombre = :nombre LIMIT 1")
+    fun getItemByName(nombre: String): LiveData<ItemEntity>
+
+    @Query("SELECT * FROM item_table WHERE id = :id")
+    fun getItemByID(id: Int): LiveData<ItemEntity>
 }
