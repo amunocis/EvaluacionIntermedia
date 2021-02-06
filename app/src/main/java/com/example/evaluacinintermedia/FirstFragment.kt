e500ffc6f8bf12a948a9f3f6ad880a7261c279c1
amunocis <amunocis@gmail.com>
2021-02-05 22:31:16 -0300

    feat: First view (fragment_first)

--- app/src/main/java/com/example/evaluacinintermedia/FirstFragment.kt
+++ app/src/main/java/com/example/evaluacinintermedia/FirstFragment.kt
@@ -7,22 +7,32 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.example.evaluacinintermedia.databinding.FragmentFirstBinding
+import android.widget.NumberPicker
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
+    private lateinit var mBinding: FragmentFirstBinding
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
+        mBinding = FragmentFirstBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return mBinding.root
+        mBinding.spCtd.minValue = 1
+        mBinding.spCtd.maxValue = 100
+        mBinding.spCtd.value = 10
+        mBinding.spCtd.displayedValues = values
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
 
         view.findViewById<Button>(R.id.button_first).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
